3109439fa279899c3ce524a93ae86fd5de921f8c
Arthur Takeda <29760526+aotakeda@users.noreply.github.com>
2020-11-06 22:25:58 -0300

    remove dead code from null.test

--- null.test.js
+++ null.test.js
@@ -1,6 +1,5 @@
 const { expect } = require('@jest/globals');
 const isNull = require('./null');
-const nullTest = require('./null');
 
 test('should be null', () => {
     expect(isNull()).toBeNull();
